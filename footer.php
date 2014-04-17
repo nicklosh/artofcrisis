c4869e54735d681bff175bde185f4677a541de35
Nick <nkmlong@gmail.com>
2014-04-17 10:44:35 +1000

    html entity of copy

--- footer.php
+++ footer.php
@@ -10,7 +10,7 @@
 
 		<div class="site-wordpress">
 				<?php if(dynamic_sidebar('crisis_footer_area')) : else : endif; ?>
-				<p>All content © <?php echo date("Y"); ?> ARTOFCRISIS.COM.AU • Site developed by <a href="http://nicklong.id.au">NICK LONG</a></p>
+				<p>All content &copy; <?php echo date("Y"); ?> ARTOFCRISIS.COM.AU • Site developed by <a href="http://nicklong.id.au">NICK LONG</a></p>
 				</div><!-- .site-info -->
 				<div class="clear"></div>
 </div><!-- #page -->
